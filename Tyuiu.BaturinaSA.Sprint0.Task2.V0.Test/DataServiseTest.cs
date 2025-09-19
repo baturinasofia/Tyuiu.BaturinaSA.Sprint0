8fcace0f7e29410a93c46977ed48b16d3f4c3156
baturinasofia <sofi1608@bk.ru>
2025-09-19 22:16:46 +0500

    Task2

---
+++ Tyuiu.BaturinaSA.Sprint0.Task2.V0.Test/DataServiseTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BaturinaSA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Софья";
+            var res = DataServise.GetMessage(name);
+
+            Assert.AreEqual("Привет, Софья", res);
+        }
+    }
+}
---
+++ Tyuiu.BaturinaSA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.BaturinaSA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataServise.GetMessage("Софья"));
+            Console.ReadKey();
+        }
+    }
+}
