4370b62b94353b537a6fcabe24d7523fa3b56bf8
baturinasofia <sofi1608@bk.ru>
2025-09-19 23:08:42 +0500

    Task3

---
+++ Tyuiu.BaturinaSA.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tyuiu.BaturinaSA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+            
+    }
+}
